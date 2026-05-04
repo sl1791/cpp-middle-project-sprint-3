48f8f36211231b10b0101ea1640694bc124c9800
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-04 12:48:55 +0300

    add: структур сравнения

--- include/comparators.hpp
+++ include/comparators.hpp
@@ -4,6 +4,52 @@
 
 namespace bookdb::comp {
 
-struct LessByAuthor {};
+struct LessByAuthor
+{
+    bool operator()(const Book& a, const Book& b) const
+    {
+        return a.author < b.author;
+    }
+};
+
+struct LessByTitle
+{
+    bool operator()(const Book& a, const Book& b) const
+    {
+        return a.title < b.title;
+    }
+};
+
+struct LessByYear
+{
+    bool operator()(const Book& a, const Book& b) const
+    {
+        return a.year < b.year;
+    }
+};
+
+struct LessByRating
+{
+    bool operator()(const Book& a, const Book& b) const
+    {
+        return a.rating > b.rating;
+    }
+};
+
+struct LessByReadCount
+{
+    bool operator()(const Book& a, const Book& b) const
+    {
+        return a.read_count > b.read_count;
+    }
+};
+
+struct LessByGenre
+{
+    bool operator()(const Book& a, const Book& b) const
+    {
+        return static_cast<int>(a.genre) < static_cast<int>(b.genre);
+    }
+}
 
 }  // namespace bookdb::comp
