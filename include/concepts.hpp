c20b4a86340bc928f30e9fbd66c64c3c5b8bf011
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-04 13:23:05 +0300

    add: добавление concepts

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -2,24 +2,56 @@
 
 #include <concepts>
 #include <iterator>
+#include <type_traits>
+#include <utility>
 
 #include "book.hpp"
 
 namespace bookdb {
 
 template <typename T>
-concept BookContainerLike = true;
+concept BookContainerLike = requires(T container, Book book)
+{
+    typename T::value_type;
+    typename T::iterator;
+    typename T::const_iterator;
+    typename T::size_type;
+
+    requires std::same_as<typename T::value_type, Book>;
+
+    { container.begin() } -> std::same_as<typename T::iterator>;
+    { container.end() } -> std::same_as<typename T::iterator>;
+
+    { std::as_const(container).begin() } -> std::same_as<typename T::const_iterator>;
+    { std::as_const(container).end() } -> std::same_as<typename T::const_iterator>;
+
+    { container.size() } -> std::same_as<typename T::size_type>;
+    { container.empty() } -> std::convertible_to<bool>;
+
+    container.clear();
+    container.emplace_back(book);
+};
 
 template <typename T>
-concept BookIterator = true;
+concept BookIterator = 
+    std::input_iterator<T> && 
+    std::same_as<std::iter_value_t<T>, Book>;
 
 template <typename S, typename I>
-concept BookSentinel = true;
+concept BookSentinel = 
+    BookIterator<I> &&
+    std::sentinel_for<S, I>;
 
 template <typename P>
-concept BookPredicate = true;
+concept BookPredicate = requires(P predicate, const Book& book)
+{
+    { predicate(book) } -> std::convertible_to<bool>;
+};
 
 template <typename C>
-concept BookComparator = true;
+concept BookComparator =  requires(C comparator, const Book& lhs, const Book& rhs)
+{
+    { comparator(lhs, rhs) } -> std::convertible_to<bool>;
+};
 
 }  // namespace bookdb
